44734f3f75145061f7fcee023ce7b5b2a0310836
jabattack1 <peter.hahn@live.com>
2015-05-22 15:32:54 -0400

    zero in progress

--- scripts.js
+++ scripts.js
@@ -49,3 +49,107 @@ function two(){
 	else {$('#answer').val(value);}
 }
 
+function three(){
+    var value = $('#three').val();
+    var place = $('#answer').val();
+
+    if(place && place < 100000) {
+    $('#answer').val(place + value);
+	}
+	else if (place > 10000) {
+		$('#three').disabled = true;
+	}
+	else {$('#answer').val(value);}
+}
+
+function four(){
+    var value = $('#four').val();
+    var place = $('#answer').val();
+
+    if(place && place < 100000) {
+    $('#answer').val(place + value);
+	}
+	else if (place > 10000) {
+		$('#four').disabled = true;
+	}
+	else {$('#answer').val(value);}
+}
+
+function five(){
+    var value = $('#five').val();
+    var place = $('#answer').val();
+
+    if(place && place < 100000) {
+    $('#answer').val(place + value);
+	}
+	else if (place > 10000) {
+		$('#five').disabled = true;
+	}
+	else {$('#answer').val(value);}
+}
+
+function six(){
+    var value = $('#six').val();
+    var place = $('#answer').val();
+
+    if(place && place < 100000) {
+    $('#answer').val(place + value);
+	}
+	else if (place > 10000) {
+		$('#six').disabled = true;
+	}
+	else {$('#answer').val(value);}
+}
+
+function seven(){
+    var value = $('#seven').val();
+    var place = $('#answer').val();
+
+    if(place && place < 100000) {
+    $('#answer').val(place + value);
+	}
+	else if (place > 10000) {
+		$('#seven').disabled = true;
+	}
+	else {$('#answer').val(value);}
+}
+
+function eight(){
+    var value = $('#eight').val();
+    var place = $('#answer').val();
+
+    if(place && place < 100000) {
+    $('#answer').val(place + value);
+	}
+	else if (place > 10000) {
+		$('#eight').disabled = true;
+	}
+	else {$('#answer').val(value);}
+}
+
+function nine(){
+    var value = $('#nine').val();
+    var place = $('#answer').val();
+
+    if(place && place < 100000) {
+    $('#answer').val(place + value);
+	}
+	else if (place > 10000) {
+		$('#nine').disabled = true;
+	}
+	else {$('#answer').val(value);}
+}
+
+function zero(){
+    var value = $('#zero').val();
+    var place = $('#answer').val();
+
+    if(place && place < 100000) {
+    $('#answer').val(place + value);
+	}
+	else if (place > 10000) {
+		$('#zero').disabled = true;
+	}
+	else {$('#answer').val(value);}
+}
+
