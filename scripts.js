59a5ff38af0f510e80b641e003b6f640cd42e731
jabattack1 <peter.hahn@live.com>
2018-05-14 18:38:04 +0900

    answer

--- scripts.js
+++ scripts.js
@@ -516,6 +516,7 @@ function division(){
 function equals(){
 	
 	if(number != null && operator === '+') {
+		console.log("go");
 	var place1 = $('#answer').val();
 	number = $('#answer').val(parseFloat(place1) + parseFloat(second));
 	first = null;
